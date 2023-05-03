740efb163390df39a8913b34e50d79918079b038
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-05-03 17:21:41 +0200

    Progressing

--- index.js
+++ index.js
@@ -11,8 +11,29 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log(req.url);
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Home</h1>');
+    console.log(`Path:`, path);
+    console.log(__dirname);
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`Error loading index.html: ${err}`);
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content);
+        }
+      }
+    );
+  }
+
+  if (req.url === '/api/users') {
+    const users = [
+      { name: 'Bob Smith', age: 40 },
+      { name: 'John Doe', age: 30 },
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
   }
 });
 
